82d1ac330bcf9b7f1a9f1b58e5f2816e79153640
Jan Cavel <caveljan@gmail.com>
2019-12-25 08:54:21 +0200

    desit-react > dev: constants | interact types

---
+++ packages/desit-react/source/constants/index.ts
@@ -0,0 +1,57 @@
+/**
+ * General interaction types to be used with `interact`.
+ * Loosely based on https://developer.mozilla.org/en-US/docs/Web/Events
+ */
+export const INTERACT_TYPES = {
+    /** Focus Events */
+    focus: 'focus',
+    blur: 'blur',
+
+    /** Form Events */
+    reset: 'reset',
+    submit: 'submit',
+
+    /** View Events */
+    fullscreenChange: 'fullscreenChange',
+    fullscreenError: 'fullscreenError',
+    resize: 'resize',
+    scroll: 'scroll',
+
+    /** Clipboard Events */
+    cut: 'cut',
+    copy: 'copy',
+    paste: 'paste',
+
+    /** Keyboard Events */
+    keyDown: 'keyDown',
+    keyPress: 'keyPress',
+    keyUp: 'keyUp',
+
+    /** Mouse Events */
+    auxiliaryClick: 'auxiliaryClick',
+    click: 'click',
+    contextMenu: 'contextMenu',
+    doubleClick: 'doubleClick',
+    mouseDown: 'mouseDown',
+    mouseEnter: 'mouseEnter',
+    mouseLeave: 'mouseLeave',
+    mouseMove: 'mouseMove',
+    mouseOver: 'mouseOver',
+    mouseOut: 'mouseOut',
+    mouseUp: 'mouseUp',
+    select: 'select',
+    wheel: 'wheel',
+
+    /** Drag & Drop Events */
+    drag: 'drag',
+    dragEnd: 'dragEnd',
+    dragEnter: 'dragEnter',
+    dragStart: 'dragStart',
+    dragLeave: 'dragLeave',
+    dragOver: 'dragOver',
+    drop: 'drop',
+
+    /** Media Events */
+    play: 'play',
+    pause: 'pause',
+}
