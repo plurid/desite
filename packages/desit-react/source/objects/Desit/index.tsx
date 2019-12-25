ea8e08a88ba81145c45e8abf41e23f94c8bf7eb9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-25 09:12:33 +0200

    desit-react > dev: Desit, graphql mutations

--- packages/desit-react/source/objects/Desit/index.tsx
+++ packages/desit-react/source/objects/Desit/index.tsx
@@ -1,4 +1,11 @@
 import {
+    ApolloClient,
+} from 'apollo-client';
+import {
+    NormalizedCacheObject,
+} from 'apollo-cache-inmemory';
+
+import {
     IDesit,
     DesitOptions,
     DesitVisitOptions,
@@ -7,20 +14,43 @@ import {
     ReactElement,
 } from '../../interfaces';
 
+import {
+    PLURID_API_ENDPOINT,
+} from '../../constants';
+
+import graphqlClient from '../../services/graphql/client';
+import {
+    DESIT_VISIT,
+    DESIT_INTERACT,
+} from '../../services/graphql/mutate';
+
 
 
 class Desit implements IDesit {
     private options: DesitOptions;
+    private client: ApolloClient<NormalizedCacheObject>;
 
     constructor(options: DesitOptions) {
         this.options = options;
+        this.client = graphqlClient(this.options.apiEndpoint || PLURID_API_ENDPOINT);
     }
 
     visit(
         path: string,
         options?: DesitVisitOptions,
     ) {
-        // record page visit
+        const inputVisitMutation = {
+            appID: this.options.appID,
+            path,
+            options,
+        };
+
+        this.client.mutate({
+            mutation: DESIT_VISIT,
+            variables: {
+                input: inputVisitMutation,
+            },
+        });
     }
 
     interact(
@@ -28,7 +58,19 @@ class Desit implements IDesit {
         element: ReactElement,
         options?: DesitInteractOptions,
     ) {
-        // record page interaction
+        const inputInteractMutation = {
+            appID: this.options.appID,
+            type,
+            element,
+            options,
+        };
+
+        this.client.mutate({
+            mutation: DESIT_INTERACT,
+            variables: {
+                input: inputInteractMutation,
+            },
+        });
     }
 }
 
--- packages/desit-react/source/services/graphql/mutate/index.ts
+++ packages/desit-react/source/services/graphql/mutate/index.ts
@@ -1,1 +1,30 @@
 import gql from 'graphql-tag';
+
+
+
+export const DESIT_VISIT = gql`
+    mutation DesitVisit($input: InputDesitVisit!) {
+        desitVisit(input: $input) {
+            status
+            errors {
+                path
+                message
+                type
+            }
+        }
+    }
+`;
+
+
+export const DESIT_INTERACT = gql`
+    mutation DesitInteract($input: InputDesitInteract!) {
+        desitInteract(input: $input) {
+            status
+            errors {
+                path
+                message
+                type
+            }
+        }
+    }
+`;
