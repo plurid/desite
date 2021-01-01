7ff5165e916cd31376814a6c9925f91242b56a09
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-01-01 21:04:24 +0200

    desite-react > setup: package

--- packages/desite-react/scripts/rollup.config.ts
+++ packages/desite-react/scripts/rollup.config.ts
@@ -1,16 +1,18 @@
-import typescript from 'rollup-plugin-typescript2';
-// import commonjs from 'rollup-plugin-commonjs';
-// import external from 'rollup-plugin-peer-deps-external';
-// import postcss from 'rollup-plugin-postcss';
-// import resolve from 'rollup-plugin-node-resolve';
-// import url from 'rollup-plugin-url';
-import replace from 'rollup-plugin-replace';
-// import babel from 'rollup-plugin-babel';
-
-import pkg from '../package.json';
+// #region imports
+    // #region libraries
+    import typescript from 'rollup-plugin-typescript2';
+    import replace from 'rollup-plugin-replace';
+    // #endregion libraries
 
 
+    // #region external
+    import pkg from '../package.json';
+    // #endregion external
+// #endregion imports
 
+
+
+// #region exports
 export default {
     input: 'source/index.ts',
     output: [
@@ -36,21 +38,10 @@ export default {
         replace({
             'process.env.ENV_MODE': JSON.stringify(process.env.ENV_MODE),
         }),
-        // external(),
-        // postcss({
-        //     modules: true
-        // }),
-        // url(),
-        // babel({
-        //     exclude: "node_modules/**"
-        // }),
-        // resolve({
-        //     modulesOnly: true,
-        // }),
         typescript({
             rollupCommonJSResolveHack: true,
             clean: true,
         }),
-        // commonjs(),
     ],
-}
+};
+// #endregion exports
