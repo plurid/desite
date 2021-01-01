c6b805008ec2a42a49f099d5f861d79a638d9204
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-01-01 21:03:20 +0200

    desite-react > setup: package

---
+++ packages/desite-react/scripts/rollup.config.ts
@@ -0,0 +1,56 @@
+import typescript from 'rollup-plugin-typescript2';
+// import commonjs from 'rollup-plugin-commonjs';
+// import external from 'rollup-plugin-peer-deps-external';
+// import postcss from 'rollup-plugin-postcss';
+// import resolve from 'rollup-plugin-node-resolve';
+// import url from 'rollup-plugin-url';
+import replace from 'rollup-plugin-replace';
+// import babel from 'rollup-plugin-babel';
+
+import pkg from '../package.json';
+
+
+
+export default {
+    input: 'source/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            exports: 'named',
+            sourcemap: true
+        }
+    ],
+    external: [
+        'cross-fetch',
+        '@apollo/client',
+        '@plurid/plurid-functions',
+    ],
+    plugins: [
+        replace({
+            'process.env.ENV_MODE': JSON.stringify(process.env.ENV_MODE),
+        }),
+        // external(),
+        // postcss({
+        //     modules: true
+        // }),
+        // url(),
+        // babel({
+        //     exclude: "node_modules/**"
+        // }),
+        // resolve({
+        //     modulesOnly: true,
+        // }),
+        typescript({
+            rollupCommonJSResolveHack: true,
+            clean: true,
+        }),
+        // commonjs(),
+    ],
+}
